d7c4f7f7806149343c79970597452c5c25daa062
Hpone-Naing <hponenaingtun@gmail.com>
2022-03-30 21:49:32 +0630

    fixed translate btn is not working in mobile

--- template/assets/js/statistic.js
+++ template/assets/js/statistic.js
@@ -82,4 +82,39 @@ searchStatistics = function(search) {
 		});
     });
 }
+
+translate = function(translateButton){
+	alert("here translate.........................")
+	var translateBtn = $(translateButton);
+	currentLanguage = translateBtn.parent().attr("lang");
+			console.log("current lang: " + currentLanguage)
+	    if(currentLanguage === "my") {
+			$("*").each(function() {
+				  currentElement = this;
+				  $.each(currentElement.attributes, function(i, attrib){
+					if(attrib.name === "lang") {
+						if (attrib.value === "en") {
+						  $(currentElement).show();
+						} else if (attrib.value === "my") {
+						  $(currentElement).hide();
+						}
+					}
+				  });
+			})
+		}else if(currentLanguage === "en") {
+			$("*").each(function() {
+				  currentElement = this;
+				  $.each(currentElement.attributes, function(i, attrib){
+					if(attrib.name === "lang") {
+						if (attrib.value === "my") {
+						  $(currentElement).show();
+						} else if (attrib.value === "en") {
+						  $(currentElement).hide();
+						}
+					}
+				  });
+			})
+
+		} 
+}
 var statistic_table = $(".statistic-table");
